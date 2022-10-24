741d23de35e304384837c2d43c2b37d888ce0005
Lautaro Matas <lmatas@gmail.com>
2022-10-24 16:47:55 +0200

    Update OldestDateFieldOccurrenceFilter.java

--- src/main/java/org/lareferencia/core/entity/indexing/filters/OldestDateFieldOccurrenceFilter.java
+++ src/main/java/org/lareferencia/core/entity/indexing/filters/OldestDateFieldOccurrenceFilter.java
@@ -4,14 +4,13 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.lareferencia.core.entity.domain.EntityRelationException;
 import org.lareferencia.core.entity.domain.FieldOccurrence;
+import org.lareferencia.core.entity.domain.SimpleFieldOccurrence;
 import org.lareferencia.core.util.date.DateHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Component
@@ -28,7 +27,10 @@ public class OldestDateFieldOccurrenceFilter implements IFieldOccurrenceFilter {
 
     public Collection<FieldOccurrence> filter(Collection<FieldOccurrence> occurrences, Map<String,String> params) {
 
+        List<FieldOccurrence> result = new ArrayList<FieldOccurrence>();
+
         int filter_limit = params.containsKey("filter-limit") ? Integer.parseInt(params.get("filter-limit")) : 1;
+        String filter_date_format = params.containsKey("filter-date-format") ? params.get("filter-date-format") : "yyyy-MM-dd";
 
         Collection<FieldOccurrence> filteredOccurrences = occurrences.stream()
                 .filter(occurrence -> getLocalDateTime(occurrence, params).equals(occurrences.stream()
@@ -40,7 +42,23 @@ public class OldestDateFieldOccurrenceFilter implements IFieldOccurrenceFilter {
         // limit the number of occurrences to filter_limit
         filteredOccurrences = filteredOccurrences.stream().limit(filter_limit).collect(Collectors.toList());
 
-        return filteredOccurrences;
+
+        for (FieldOccurrence fo : filteredOccurrences) {
+
+            LocalDateTime ldt = getLocalDateTime(fo, params);
+            // create a new FieldOccurrence in replace of the existing one, the new one is built by selected subfields formatted as filterFormat
+            FieldOccurrence newOccur = new SimpleFieldOccurrence(fo.getFieldType());
+
+            try {
+                newOccur.addValue( DateHelper.getDateTimeFormattedString(ldt, filter_date_format) );
+                result.add(newOccur);
+            } catch (EntityRelationException e) {
+                logger.error("Error filtering date occurrences " + this.getName() + " ", e);
+            }
+
+        }
+
+        return result;
     }
 
     // TODO: move this to a helper class
